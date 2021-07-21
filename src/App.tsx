c93b7e90a443ce8e06917e9a193eab33e643dbc7
ergincetin <ergincetn01@gmail.com>
2021-07-21 14:26:15 +0300

    router good for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import { Route, Router, Switch } from "react-router";
+// import {  } from "react-router";
 import Layout, { Content, Header } from "antd/lib/layout/layout";
 import Navbar from "./Components/Menu/NavBar";
 import { Home } from "./Components/Menu/Home";
 import { createBrowserHistory } from "history";
 import { Page2 } from "./Components/Menu/Page2";
+import { BrowserRouter  as Router, Switch,Route, Link} from "react-router-dom";
+import Page3 from "./Components/Menu/Page3";
 
 const history = createBrowserHistory();
 
@@ -16,12 +18,13 @@ const App: React.FC = () => {
           <Navbar />
         </Header>
         <Content>
-          <Router history={history}>
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/page2" component={Page2} />
-            {/* <Route exact path="/home" component={Home} /> */}
+          <Router>
+            {/* this block hinders links and renders working  synchronized.
+             */}
           </Router>
-          
+          <Route strict path="/home" component={Home} />
+          <Route exact path="/page2" component={Page2} />
+          <Route exact path="/page3" component={Page3} />
         </Content>
       </Layout>
     </div>
--- src/Components/Menu/Page2.tsx
+++ src/Components/Menu/Page2.tsx
@@ -1,11 +1,144 @@
+import { Button } from "antd";
 import Layout, { Content } from "antd/lib/layout/layout";
 import React from "react";
 
+let mystr:string;
+mystr="cdwqvdvwdvwdvwvw";
+
+
+// enum list {
+
+// }
+
+// interface User {
+//     name: string;
+//     id: number;
+//   }
+  
+//   class UserAccount {
+//     name: string;
+//     id: number;
+  
+//     constructor(name: string, id: number) {
+//       this.name = name;
+//       this.id = id;
+//     }
+//   }
+  
+//   const user: User = new UserAccount("Murphy", 1);
+
+//   function useUser(user: User):string {
+    
+//       let newuser = new UserAccount("ergin", 5);
+//       newuser = user;
+//       return newuser.name;
+//   }
+// type R = "rear";
+// type Gear = 1 | 2 | 3 | 4 | 5 | R;
+
+enum Gear {
+    one= 1,
+    two, 
+    three,
+    four, five, 
+    rear= "r",
+
+}
+
  export const Page2:React.FC =()=> {
+
+    function condit(){
+        let myobj = 4;
+        if(typeof myobj==="string"){
+            return "string";
+        }
+        else {
+            return "not string";
+        }
+    }
+function toLower(input:string):string {
+    return input.toLowerCase();
+}
+
+function printScreen( a:string) {
+    alert(a);
+}
+
+
+const user = {
+    name: "ergin",
+    id: 2177,
+}
+
+interface User {
+    name: string;
+    id: number;
+}
+
+class UserInfo {
+    name: string;
+    id: number;
+
+    constructor(name:string, id:number) {
+        this.name=name;
+        this.id=id;
+    }
+}
+
+interface First {
+    name: string;
+    age:number;
+    isHere:boolean;
+    year: number;
+}
+
+interface Second extends First {
+    surname: string;
+    isDone: boolean;
+
+}
+class FirstSec {
+    isDone: boolean;
+    name: string;
+    surname:string;
+    age:number;
+    isHere:boolean;
+    year: number;
+
+    constructor(isDone: boolean, name:string, age:number, isHere: boolean, year:number, surname:string){
+        this.isDone=isDone;
+        this.name= name;
+        this.age=age;
+        this.surname=surname;
+    this.isHere=isHere;
+    this.year=year;
+    }
+}
+
+const derf:Second = new FirstSec(true, "ergin", 21, true, 2019556021, "Cetin");
+
+     const user1:User = new UserInfo("ergin", 2144);
+
      return (
          <Layout>
              <Content>
-                 Page2
+                 
+                 {mystr}
+                 <br/>
+                 {Gear.rear}
+                 {condit()} <br/>
+                 {toLower("FBFBDSWWBFBFDB")} <br/>
+                 <Button style={{color: "red"}} onClick={
+                     ()=>printScreen("ergin")}></Button>
+                 <br/>
+                 {user1.id}
+                 <br/>
+                 {user.name}
+                 <br/>
+                 {derf.surname}
+                 
+                 {/* {user.id} - {user.name}
+                 {useUser(user)} */}
              </Content>
 
          </Layout> 
